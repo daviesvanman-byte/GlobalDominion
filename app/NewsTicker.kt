f936ca48d1c5e24e28d179acb7a3b9831093c466
daviesvanman-byte <daviesvanman@gmail.com>
2026-01-15 15:20:53 +0000

    Create NewsTicker.kt

---
+++ app/NewsTicker.kt
@@ -0,0 +1,10 @@
+package com.globaldominion.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NewsTicker() {
+    // TODO: Implement cartoon-style news ticker
+    Text("News Ticker Placeholder: Major events will appear here.")
+}
