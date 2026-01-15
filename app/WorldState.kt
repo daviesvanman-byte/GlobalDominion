2a1aeb2234c90d0908fd8a9509f7532a4fb4fe81
daviesvanman-byte <daviesvanman@gmail.com>
2026-01-15 15:21:21 +0000

    Create WorldState.kt

---
+++ app/WorldState.kt
@@ -0,0 +1,8 @@
+package com.globaldominion.core
+
+import com.globaldominion.data.Country
+
+class WorldState {
+    val countries: MutableList<Country> = mutableListOf()
+    // TODO: Implement global simulation state including alliances, treaties, disasters, economy
+}
